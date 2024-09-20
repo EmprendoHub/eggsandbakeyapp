b5c4a7b4545b2464eada7011648146eb87fa2475
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2024-09-20 09:09:36 -0600

    almost there

--- src/app/components/forms/GoogleCaptchaWrapper.tsx
+++ src/app/components/forms/GoogleCaptchaWrapper.tsx
@@ -2,7 +2,11 @@
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import React from "react";
 
-export default function GoogleCaptchaWrapper({ children }: { children: any }) {
+export default function GoogleCaptchaWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const recaptchaKey = process?.env?.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
   return (
     <GoogleReCaptchaProvider

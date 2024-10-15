4a28db5597a33271143fce1bcd7c3a6bf8d3d84e
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2024-10-15 09:10:39 -0600

    native video update

--- src/app/components/HeroLoader/Hero.tsx
+++ src/app/components/HeroLoader/Hero.tsx
@@ -1,16 +1,10 @@
 "use client";
 import { motion } from "framer-motion";
 import "./hero.css";
-import ReactPlayer from "react-player";
 import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 
 const Hero = ({ shouldPlay }: { shouldPlay: boolean }) => {
-  const titleVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 1 } },
-  };
-
   // Pulsating animation for the ChevronDown
   const chevronVariants = {
     pulse: {
@@ -26,24 +20,18 @@ const Hero = ({ shouldPlay }: { shouldPlay: boolean }) => {
 
   return (
     <section className="hero overflow-hidden flex flex-col justify-center items-center bg-[#0e0f0e]">
-      <motion.div
-        className="flex mt-20 items-center justify-center h-full w-full"
-        initial="hidden"
-        animate="visible"
-        variants={titleVariants}
-      >
-        <ReactPlayer
-          url="/videos/EggsBakeyIntro.mp4"
+      <div className="flex mt-20 items-center justify-center h-full w-full">
+        <video
           width="100%"
           height="100%"
           controls={false}
-          playing={shouldPlay} // Only play when loader finishes
+          autoPlay={shouldPlay} // Only play when loader finishes
           muted={true}
           playsInline={true}
-          disableRemotePlayback={true}
-          preload="auto" // Preload the video
-        />
-      </motion.div>
+        >
+          <source src="/videos/EggsBakeyIntro.mp4" type="video/mp4" />
+        </video>
+      </div>
       <Link href={"#start"} className="absolute z-30 bottom-0">
         <motion.div variants={chevronVariants} animate="pulse">
           <ChevronDown size={100} />

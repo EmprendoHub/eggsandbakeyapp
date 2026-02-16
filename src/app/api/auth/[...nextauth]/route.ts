92c3756e4a80d032835aad8b822be23ddf8b4791
Salvador <54673042+EmprendoHub@users.noreply.github.com>
2026-02-16 01:22:02 -0600

    lazy load changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,13 @@ import type { NextRequest } from "next/server";
 import NextAuth from "next-auth";
 
 export async function GET(request: NextRequest, context: unknown) {
-	const { authOptions } = await import("@/lib/auth");
-	const handler = NextAuth(authOptions);
-	return handler(request, context);
+  const { authOptions } = await import("@/lib/auth");
+  const handler = NextAuth(authOptions);
+  return handler(request, context);
 }
 
 export async function POST(request: NextRequest, context: unknown) {
-	const { authOptions } = await import("@/lib/auth");
-	const handler = NextAuth(authOptions);
-	return handler(request, context);
+  const { authOptions } = await import("@/lib/auth");
+  const handler = NextAuth(authOptions);
+  return handler(request, context);
 }
